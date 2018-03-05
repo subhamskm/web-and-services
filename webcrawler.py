d25a9c1d9a7fcf7ea047d96f3c02b5ddb266a8bc
Kumar <34918826+kumar-spy@users.noreply.github.com>
2018-03-05 17:04:11 +0530

    solved the reference problem

--- webcrawler.py
+++ webcrawler.py
@@ -6,46 +6,49 @@ class decode():
         self.fhand=fhand
     def decode(self):   #saves the response to the specified file and path
         os.chdir(self.path)
-        os.mkdir("Decode")
-        os.chdir(self.path+"//Decode")
-        self.file = open("decode.txt", "w")
+        if not os.path.exists(path+"\\Decode"):
+            os.mkdir("Decode")
+        else:
+            os.chdir(self.path+"\\Decode")
+            file = open("decode.txt", "w")
         for i in self.fhand:
             line=i.decode()
-            self.file.write(line)
-        self.file.close()
+            file.write(line)
+        file.close()
 class Webcrawler(decode):
     #class to different function on web crawling , superclass=  decode()
-    #def check(self):
-        #if self.fhand==None:
-            #print("Enter valid Url")
-            #exit(1)
+    def check(self):
+        os.chdir(path + "\\Decode")
+        if self.fhand==None:
+            print("Enter valid Url")
+            exit(1)
     def Link_crawl(self,file_name=" "):    #link crawler
-        self.file=open(self.path+"//Decode//decode.txt","r")
-        for line in self.file:
+        file=open(self.path+"\\Decode\\Decode.txt","r")
+        file_name = open(file_name, "a")
+        for line in file:
             found_string=re.findall(r'href="[\w\W\S\s.]{1,80}">',line)
-            self.file_name=open(self.path.join(self.path+"\\"+file_name),"a")
             for j in found_string:
-                self.file.write(j[6:]+"\b\n")
-        self.file_name.close()
-        self.file.close()
+                file_name.write(j[6:]+"\b\n")
+        file_name.close()
+        file.close()
     def Name_crawl(self,file_name=" "): #Name crawler
-        self.file = open(path + "//Decode//decode.txt", "r")
-        for line in self.file:
-            self.found_string=re.findall(r'[A-Z]{3,15}[\s]')
-            self.file_name = open(self.path.join(self.path + "\\" + file_name), "a")
-            for j in self.found_string:
-                self.file.write(j+ "\n")
-        self.file_name.close()
-        self.file.close()
+        file = open(path + "\\Decode\\Decode.txt", "r")
+        file_name = open(file_name, "a")
+        for line in file:
+            found_string=re.findall(r'[A-Z]{3,15}[\s]',line)
+            for j in found_string:
+                file_name.write(j+ "\n")
+        file_name.close()
+        file.close()
     def Contact_number(self,file_name=" "):     #contact crawler
-        self.file = open(path + "//Decode//decode.txt", "r")
-        for line in  self.file:
-            self.found_string=re.findall(r'[+(]{1}[\d]{1}[)]{1}[\d\s]{1,10}[\d]',line)
-            self.file_name = open(self.path.join(self.path + "\\" + file_name), "a")
-            for j in self.found_string:
-                self.file.write(j + "\n")
-        self.file_name.close()
-        self.file.close()
+        file = open(self.path + "\\Decode\\Decode.txt", "r")
+        file_name = open(file_name, "a")
+        for line in  file:
+            found_string=re.findall(r'[+(]{1}[\d]{1}[)]{1}[\d\s]{1,10}[\d]',line)
+            for j in found_string:
+                file.write(j + "\n")
+        file_name.close()
+        file.close()
 while True:
     #url="https://www.regular-expressions.info/python.html"#str(input())
     fhand = urllib.request.urlopen("https://www.regular-expressions.info/python.html")
@@ -54,12 +57,13 @@ while True:
     obj_decode.decode()
     obj_crawl=Webcrawler()          #object of Webcrawler class
     #obj_crawl.check()
-    num=input("0: Link_crawl 1:  Name_crawl  2: Contact_number e : exit\n")
+    num=int(input("0: Link_crawl 1:  Name_crawl  2: Contact_number e : exit\n"))
+    print(num)
     if num==0:
-        obj_crawl.Link_crawl()
+        obj_crawl.Link_crawl("links.txt")
     elif num==1:
-        obj_crawl.Name_crawl()
+        obj_crawl.Name_crawl("name.txt")
     elif num==2:
-        obj_crawl.Contact_number()
+        obj_crawl.Contact_number("contact.txt")
     else:
         exit(1)
